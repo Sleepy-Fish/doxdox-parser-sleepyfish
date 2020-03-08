f707bc980d7c6f04fca2c69c17cd3da263bb3553
JRileyH <riley@sleepy.fish>
2020-03-08 14:03:11 -0400

    added better lintingh

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const dox = require('dox');
  */
 
 const formatStringForName = content =>
-    content.toString().replace(/module\.exports\.|\.prototype|\(\)/gu, '');
+  content.toString().replace(/module\.exports\.|\.prototype|\(\)/gu, '');
 
 /**
  * Format string as param.
@@ -22,7 +22,7 @@ const formatStringForName = content =>
  */
 
 const formatStringForParam = content =>
-    content.toString().replace(/\[|\]/gu, '');
+  content.toString().replace(/\[|\]/gu, '');
 
 /**
  * Format string as UID.
@@ -34,11 +34,11 @@ const formatStringForParam = content =>
  */
 
 const formatStringForUID = content =>
-    content
-        .toString()
-        .toLowerCase()
-        .replace(/[^\w\.]+/gu, '-')
-        .replace(/^-|-$/gu, '');
+  content
+    .toString()
+    .toLowerCase()
+    .replace(/[^\w.]+/gu, '-')
+    .replace(/^-|-$/gu, '');
 
 /**
  * Dox parser for doxdox.
@@ -51,72 +51,64 @@ const formatStringForUID = content =>
  */
 
 const parser = (content, filename) =>
-    dox
-        .parseComments(content, {
-            'raw': true,
-            'skipSingleStar': true
+  dox
+    .parseComments(content, {
+      raw: true,
+      skipSingleStar: true
+    })
+    .filter(method => !method.ignore && method.ctx)
+    .map(method => ({
+      uid: formatStringForUID(`${filename}-${method.ctx.string}`),
+      isPrivate: method.isPrivate,
+      isAbstract: Boolean(method.tags.filter(tag => tag.type === 'abstract').length),
+      type: method.ctx.type,
+      name: formatStringForName(method.ctx.string),
+      description: method.description.full,
+      empty: !method.description.full && !method.tags.length,
+      params: method.tags
+        .filter(tag => tag.type === 'param' && !tag.name.match(/\./u))
+        .map(tag => {
+          if (tag.optional) {
+            return `[${formatStringForParam(tag.name)}]`;
+          }
+          return formatStringForParam(tag.name);
         })
-        .filter(method => !method.ignore && method.ctx)
-        .map(method => ({
-            'uid': formatStringForUID(`${filename}-${method.ctx.string}`),
-            'isPrivate': method.isPrivate,
-            'isAbstract': Boolean(method.tags
-                .filter(tag => tag.type === 'abstract').length),
-            'type': method.ctx.type,
-            'name': formatStringForName(method.ctx.string),
-            'description': method.description.full,
-            'empty': !method.description.full && !method.tags.length,
-            'params': method.tags
-                .filter(tag => tag.type === 'param' && !tag.name.match(/\./u))
-                .map(tag => {
-
-                    if (tag.optional) {
-
-                        return `[${formatStringForParam(tag.name)}]`;
-
-                    }
-
-                    return formatStringForParam(tag.name);
-
-                })
-                .join(', ')
-                .replace(/\], \[/gu, ', ')
-                .replace(', [', '[, '),
-            'tags': {
-                'example': method.tags
-                    .filter(tag => tag.type === 'example')
-                    .map(tag => tag.string),
-                'param': method.tags
-                    .filter(tag => tag.type === 'param')
-                    .map(tag => ({
-                        'name': formatStringForParam(tag.name),
-                        'isOptional': tag.optional,
-                        'types': tag.types,
-                        'description': tag.description
-                    })),
-                'property': method.tags
-                    .filter(tag => tag.type === 'property')
-                    .map(tag => ({
-                        'name': tag.name,
-                        'types': tag.types,
-                        'description': tag.description
-                    })),
-                'extends': method.tags
-                    .filter(tag => tag.type === 'extends' ||
-                            tag.type === 'augments')
-                    .map(tag => ({
-                        'type': tag.type,
-                        'class': tag.otherClass
-                    })),
-                'return': method.tags
-                    .filter(tag => tag.type === 'return' ||
-                        tag.type === 'returns')
-                    .map(tag => ({
-                        'types': tag.types,
-                        'description': tag.description
-                    }))
-            }
-        }))
-        .filter(method => !method.empty);
+        .join(', ')
+        .replace(/\], \[/gu, ', ')
+        .replace(', [', '[, '),
+      tags: {
+        example: method.tags
+          .filter(tag => tag.type === 'example')
+          .map(tag => tag.string),
+        param: method.tags
+          .filter(tag => tag.type === 'param')
+          .map(tag => ({
+            name: formatStringForParam(tag.name),
+            isOptional: tag.optional,
+            types: tag.types,
+            description: tag.description
+          })),
+        property: method.tags
+          .filter(tag => tag.type === 'property')
+          .map(tag => ({
+            name: tag.name,
+            types: tag.types,
+            description: tag.description
+          })),
+        extends: method.tags
+          .filter(tag => tag.type === 'extends' || tag.type === 'augments')
+          .map(tag => ({
+            type: tag.type,
+            class: tag.otherClass
+          })),
+        return: method.tags
+          .filter(tag => tag.type === 'return' || tag.type === 'returns')
+          .map(tag => ({
+            types: tag.types,
+            description: tag.description
+          }))
+      }
+    }))
+    .filter(method => !method.empty);
 
 module.exports = parser;
--- test/specs/parser.js
+++ test/specs/parser.js
@@ -2,28 +2,16 @@ const assert = require('assert');
 const fs = require('fs');
 
 const parser = require('../../index');
+const files = ['dox', 'facade'];
 
-describe('doxdox parser', () => {
+describe('doxdox parser', function () {
+  for (const file of files) {
+    it(`run dox parser on ${file}.js`, function () {
+      const contents = fs.readFileSync(`./test/fixtures/${file}.js`, 'utf8');
+      const methods = parser(contents, `${file}.js`);
+      const data = fs.readFileSync(`./test/fixtures/${file}.json`, 'utf8');
 
-    [
-        'dox',
-        'facade'
-    ].forEach(file => {
-
-        it(`run dox parser on ${file}.js`, () => {
-
-            const contents = fs.readFileSync(`./test/fixtures/${file}.js`, 'utf8');
-
-            const methods = parser(contents, `${file}.js`);
-
-            // fs.writeFileSync(`./test/fixtures/${file}.json`, JSON.stringify(methods, true, 4));
-
-            const data = fs.readFileSync(`./test/fixtures/${file}.json`, 'utf8');
-
-            assert.deepEqual(methods, JSON.parse(data));
-
-        });
-
+      assert.deepStrictEqual(methods, JSON.parse(data));
     });
-
+  }
 });
